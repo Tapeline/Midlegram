390f1ff6a303a1b7aeeb13a4e916d23314b5fc88
Tapeline <mail@tapeline.dev>
2025-12-22 16:35:37 +0500

    Fix for 1-byte chars

--- mg-server/midlegram/domain/text_adapter.py
+++ mg-server/midlegram/domain/text_adapter.py
@@ -3,6 +3,8 @@ def adapt_string(text: str) -> str:
     return "".join(filter(_is_valid_char_for_java, chars))
 
 def _is_valid_char_for_java(c: bytes) -> bool:
+    if c[0] <= 127:
+        return True
     if 192 <= c[0] <= 223 and (len(c) < 2 or 128 <= c[1] <= 191):
         return False
     if 224 <= c[0] <= 239 and (len(c) < 2 or 128 <= c[1] <= 191):
